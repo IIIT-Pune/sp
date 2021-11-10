670e3a00b852a91f1df4778889eb0d2edd0e2a99
Pkr B <pkrb007@gmail.com>
2021-11-10 18:58:11 +0530

    Improving code readability

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,50 +2,47 @@ import { Typography, Button, Box } from "@material-ui/core";
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
-import { getAuth, GoogleAuthProvider, signInWithPopup, } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const google_provider = new GoogleAuthProvider();
 const auth = getAuth();
 
 const useStyles = makeStyles({
-  loginHeading: {
-    margin: "1%",
-  },
-  btnStyling: {
-    marginTop: "1%",
-    textAlign: "left",
-    background: "#3e57ab",
-    color: "white"
-  },
+	loginHeading: {
+		margin: "1%",
+	},
+	btnStyling: {
+		marginTop: "1%",
+		textAlign: "left",
+		background: "#3e57ab",
+		color: "white",
+	},
 });
 
 export default function Login() {
-  const classes = useStyles();
-  const history= useHistory();
+	const classes = useStyles();
+	const history = useHistory();
 
-  const SignInwithFirebase = () => {
-    ;
-    signInWithPopup(auth, google_provider)
-      .then(() => {
-        history.push("./Form")
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
+	const SignInwithFirebase = () => {
+		signInWithPopup(auth, google_provider)
+			.then(() => {
+				history.push("./Form");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  return (
-    <div>
-      <Typography className={classes.loginHeading} variant="h3" align="center">
-        Login
-      </Typography>
-      <Typography align="center">
-        Login using institute email id only.
-      </Typography>
-      <Box textAlign="center">
-        <Button className={classes.btnStyling} variant="contained" onClick={SignInwithFirebase}>
-          SIGN IN WITH GOOGLE
-        </Button>
-      </Box>
-    </div>
-  );
+	return (
+		<div>
+			<Typography className={classes.loginHeading} variant='h3' align='center'>
+				Login
+			</Typography>
+			<Typography align='center'>Login using institute email id only.</Typography>
+			<Box textAlign='center'>
+				<Button className={classes.btnStyling} variant='contained' onClick={SignInwithFirebase}>
+					SIGN IN WITH GOOGLE
+				</Button>
+			</Box>
+		</div>
+	);
 }
